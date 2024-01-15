a3389b7f761d0a1c558554d9774eaff3a116b243
m0libdEn <danilarogozhnikov@gmail.com>
2024-01-14 20:56:14 -0500

    feat: organizing scss + add script for scroll

---
+++ js/scroll.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var headerOptions = document.querySelectorAll('.header__option');
+    var headerLine = document.querySelector('.header__wrapper');
+    function handleScroll() {
+        var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+        // Условие для изменения цвета
+        if (scrollPosition > 650) {
+            headerOptions.forEach(function(option) {
+                option.classList.add('scrolled-color');
+            });
+            headerLine.classList.add('scrolled-line')
+        } else {
+            headerOptions.forEach(function(option) {
+                option.classList.remove('scrolled-color');
+            });
+            headerLine.classList.remove('scrolled-line')
+        }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+});
